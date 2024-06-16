23edaa597e0ddd7b45ecaf370c0bd26936515844
Wing Lee <contact@winglee.io>
2024-06-16 16:01:56 -0700

    Add documentation app

---
+++ apps/documentation/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span>Aether</span>,
+    project: {
+        link: "https://github.com/wingleeio/aether",
+    },
+    docsRepositoryBase: "https://github.com/wingleeio/aether/apps/documentation",
+};
