a8b367c85a26a9fa94333a3bc29ce5f153c0954b
Wing Lee <contact@winglee.io>
2024-06-19 19:30:29 -0700

    Update theme.config.jsx

--- apps/documentation/theme.config.jsx
+++ apps/documentation/theme.config.jsx
@@ -6,7 +6,18 @@ export default {
     docsRepositoryBase: "https://github.com/wingleeio/aetheris/tree/main/apps/documentation",
     useNextSeoProps() {
         return {
-          titleTemplate: '%s – Aetheris'
-        }
-      }
+            titleTemplate: "%s – Aetheris",
+        };
+    },
+    footer: {
+        text: (
+            <span>
+                MIT {new Date().getFullYear()} ©{" "}
+                <a href="https://docs.aetheris.io" target="_blank">
+                    Aetheris
+                </a>
+                .
+            </span>
+        ),
+    },
 };
